fd8aae3bb1fd220746d30ee5469b589d4d7376b4
blasson42 <brycelasson@gmail.com>
2024-11-05 23:16:48 -0700

    Implemented get, create, and clear for SQLAuthDAO

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,22 +1,51 @@
 package dataaccess;
 
 import model.AuthData;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SQLAuthDAO implements IAuthDAO {
 
     @Override
     public void clear() throws DataAccessException {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        String sql = "DELETE FROM AuthTokens";
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error clearing AuthToken table: " + e.getMessage());
+        }
     }
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        String sql = "INSERT INTO AuthTokens (authToken, username) VALUES (?, ?)";
+        try (Connection conn = DatabaseManager.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, auth.authToken());
+            stmt.setString(2, auth.username());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error creating auth token: " + e.getMessage());
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        String sql = "SELECT authToken, username FROM AuthTokens WHERE authToken = ?";
+        try (Connection conn = DatabaseManager.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, authToken);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new AuthData(rs.getString("authToken"), rs.getString("username"));
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DataAccessException("Error getting auth token: " + e.getMessage());
+        }
     }
 
     @Override
