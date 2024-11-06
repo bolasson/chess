cd6ecd298c0909eb2d9ae005633c05db395831cc
blasson42 <brycelasson@gmail.com>
2024-11-05 23:09:04 -0700

    Created frameworks for tests for SQLAuthDAO

--- server/src/test/java/dataaccess/SQLAuthDAOTests.java
+++ server/src/test/java/dataaccess/SQLAuthDAOTests.java
@@ -1,4 +1,40 @@
 package dataaccess;
 
+import org.junit.jupiter.api.Test;
+
 public class SQLAuthDAOTests {
+    @Test
+    public void clearSuccess() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    public void createAuthSuccess() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    public void createAuthFailureDuplicateToken() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    public void getAuthSuccess() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    public void getAuthFailureDoesNotExist() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    public void deleteAuthSuccess() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Test
+    public void deleteAuthFailureDoesNotExist() throws DataAccessException {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
 }
