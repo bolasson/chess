3e87263383a4aa30e5a0d80dd380565f4cbf4d36
blasson42 <brycelasson@gmail.com>
2025-01-11 11:44:16 -0700

    Implemented Chess Position

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,8 +7,45 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int row;
+    private final int col;
 
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + col;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String returnValue = "";
+        switch (col) {
+            case 1 -> returnValue = "A";
+            case 2 -> returnValue = "B";
+            case 3 -> returnValue = "C";
+            case 4 -> returnValue = "D";
+            case 5 -> returnValue = "E";
+            case 6 -> returnValue = "F";
+            case 7 -> returnValue = "G";
+            case 8 -> returnValue = "H";
+        };
+        returnValue += String.valueOf(row);
+        return returnValue;
     }
 
     /**
@@ -16,7 +53,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +61,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }
