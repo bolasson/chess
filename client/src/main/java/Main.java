7f84a3659d13a4565107be822fe231f2f234a155
blasson42 <brycelasson@gmail.com>
2025-03-24 14:48:00 -0600

    Added a basic version of REPL to Main.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import chess.*;
+import client.REPL;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverURL = "http://localhost:8080";
+        if (args.length > 0) {
+            serverURL = args[0];
+        }
+        new REPL(serverURL).run();
     }
 }
